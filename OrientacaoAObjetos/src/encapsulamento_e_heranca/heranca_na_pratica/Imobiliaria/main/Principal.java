6cfe579ed7be571228e3148fda702a8611b5ba21
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2023-06-13 17:36:05 -0300

    :sparkles: feat: Implementa a classe "Principal.java".

--- OrientacaoAObjetos/src/encapsulamento_e_heranca/heranca_na_pratica/Imobiliaria/main/Principal.java
+++ OrientacaoAObjetos/src/encapsulamento_e_heranca/heranca_na_pratica/Imobiliaria/main/Principal.java
@@ -1,10 +1,23 @@
 package encapsulamento_e_heranca.heranca_na_pratica.Imobiliaria.main;
 
+import java.text.NumberFormat;
+import java.time.LocalDate;
+
+import encapsulamento_e_heranca.heranca_na_pratica.Imobiliaria.classes.Aluguel;
+import encapsulamento_e_heranca.heranca_na_pratica.Imobiliaria.classes.Venda;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Aluguel apto1 = new Aluguel("Rua Aclimação, 32", LocalDate.now(), 1800);
+		System.out.println("Endereço: " + apto1.getEndereco());
+		System.out.println("Valor aluguel: " + NumberFormat.getCurrencyInstance().format(apto1.getValorAluguel()));
+		
+		Venda apto2 = new Venda("Rua Consolação, 123", LocalDate.now(), 600000);
+		System.out.println("\nEndereço: " + apto2.getEndereco());
+		System.out.println("Valor venda: " + NumberFormat.getCurrencyInstance().format(apto2.getValorVenda()));
+		
 	}
 
 }
