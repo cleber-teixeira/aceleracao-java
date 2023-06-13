de04cc2f6541c6154553774ca5e99509ebbe66bd
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2023-06-13 16:16:08 -0300

    :sparkles: feat: Implementa a classe "Aluguel.java".

--- OrientacaoAObjetos/src/encapsulamento_e_heranca/heranca_na_pratica/Imobiliaria/classes/Aluguel.java
+++ OrientacaoAObjetos/src/encapsulamento_e_heranca/heranca_na_pratica/Imobiliaria/classes/Aluguel.java
@@ -1,5 +1,38 @@
 package encapsulamento_e_heranca.heranca_na_pratica.Imobiliaria.classes;
 
-public class Aluguel {
+import java.time.LocalDate;
 
+/**
+ * A classe "Aluguel" é filha da classe "Imovel", ou seja, a classe "Aluguel" recebe por 'herança' todos os atributos da classe "Imovel".
+ */
+public class Aluguel extends Imovel {
+
+	// atributos da classe Aluguel.
+	private double valorAluguel;
+	private boolean alugado;
+
+	// método construtor
+	public Aluguel(String endereco, LocalDate dataCadastro) {
+		super(endereco, dataCadastro);
+		
+		this.alugado = false;
+	}
+
+	public double getValorAluguel() {
+		return valorAluguel;
+	}
+
+	public void setValorAluguel(double valorAluguel) {
+		this.valorAluguel = valorAluguel;
+	}
+
+	public boolean isAlugado() {
+		return alugado;
+	}
+
+	public void alugar() {
+		this.alugado = true;
+	}
+	
+	
 }
