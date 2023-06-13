7f20f1c340022c36d25fbf0482d5d067b08dae0b
DESKTOP-JV7IJVI\Cleber <cleberlt@gmail.com>
2023-06-13 16:08:23 -0300

    :sparkles: feat: Implementa a classe "Imovel.java".

--- OrientacaoAObjetos/src/encapsulamento_e_heranca/heranca_na_pratica/Imobiliaria/classes/Imovel.java
+++ OrientacaoAObjetos/src/encapsulamento_e_heranca/heranca_na_pratica/Imobiliaria/classes/Imovel.java
@@ -1,5 +1,43 @@
 package encapsulamento_e_heranca.heranca_na_pratica.Imobiliaria.classes;
 
+import java.time.LocalDate;
+
 public class Imovel {
 
+	private String endereco;
+	private LocalDate dataCadastro;
+	private boolean ativo;
+	
+	public Imovel(String endereco, LocalDate dataCadastro) {
+		super();
+		this.endereco = endereco;
+		this.dataCadastro = dataCadastro;
+		this.ativo = false;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void ativarCadastro() {
+		this.ativo = true;
+	}
+	
+	
 }
